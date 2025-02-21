0313c8aa73265504f88770c3c37b2aff7a3f7479
=basantaisg <basantaparajuli.11@gmail.com>
2025-02-21 15:51:49 +0545

    changesi n modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
+  imports: [
+    PrismaService,
+    JwtModule.register({
+      secret: 'basanta!', // Replace with environment variable later
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
 })
 export class AuthModule {}
