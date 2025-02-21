02ef32604173d8999008e9ac0e28eb075e9f409c
=basantaisg <basantaparajuli.11@gmail.com>
2025-02-21 15:45:50 +0545

    protecting routes!

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginuserDto } from './dto/login-user.dto';
+import { Response } from 'express';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async signup(@Body() createUserDto: CreateUserDto) {
+    return this.authService.signup(createUserDto);
+  }
+
+  @Post('login')
+  async login(@Body() loginDto: LoginuserDto, @Res() res: Response) {
+    const user = await this.authService.validateUser(loginDto);
+    if (!user) {
+      return new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
+    }
+
+    const token = await this.authService.login(user);
+    res.cookie('jwt', token.access_token, { httpOnly: true });
+    return res.json({ message: 'Logged in successfully!' });
+  }
+}
