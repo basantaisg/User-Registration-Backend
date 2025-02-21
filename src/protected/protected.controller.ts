8600b83f15c0a3ad9867596ce3fedb2231fd57be
=basantaisg <basantaparajuli.11@gmail.com>
2025-02-21 15:47:16 +0545

    implementation of protected routes!

--- src/protected/protected.controller.ts
+++ src/protected/protected.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { JWTAuthGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('protected')
-export class ProtectedController {}
+export class ProtectedController {
+  @UseGuards(JWTAuthGuard)
+  @Get()
+  getProtectedRoute() {
+    return { message: 'This is a protected route!' };
+  }
+}
